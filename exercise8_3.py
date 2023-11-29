7c042839ecf9fd94717452c4b3da8330dc091c21
RovSveta <152397866+RovSveta@users.noreply.github.com>
2023-11-29 14:29:32 +0200

    exercise 8_3 cleaned up

--- exercise8_3.py
+++ exercise8_3.py
@@ -6,13 +6,11 @@ if user_color == 1:
 elif user_color == 2:
     color = (0, 128, 0)
 elif user_color == 3:
-    color = (0,0,255)
+    color = (0, 0, 255)
 else:
     color = (255, 255, 0)
 
-img = Image.new('RGB', (500, 300), color = color)
-
-
+img = Image.new('RGB', (500, 300), color=color)
 
 d = ImageDraw.Draw(img)
 
@@ -23,10 +21,8 @@ text_user = input("Please, give some text here:\n")
 
 d.text((30, 250), text_user, font=fnt, fill=(255, 165, 0))
 
-d.polygon([(200,100), (400, 100), (300,240)], fill = (255,255,255))
+d.polygon([(200, 100), (400, 100), (300, 240)], fill=(255, 255, 255))
 
-d.ellipse((250, 100, 350, 200), fill=(0,0,255), outline=(0, 0, 0))
-
-
+d.ellipse((250, 100, 350, 200), fill=(0, 0, 255), outline=(0, 0, 0))
 
 img.save('pil_text.png')
